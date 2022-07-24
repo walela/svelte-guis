96f5b8b5e135bb199c5481e376c25e63751769a6
Austin Walela <austinmasibo@gmail.com>
2022-07-24 12:43:13 +0300

    tweak heading

--- apps/src/App.svelte
+++ apps/src/App.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <main class="w-3/4 mx-auto my-6 py-4">
-  <h1 class="text-3xl text-center text-gray-700 extrabold mb-4" >Hello, Svelte!</h1>
+  <h1 class="text-3xl text-center text-gray-700 extrabold mb-4">Svelte GUIs</h1>
   <Counter />
 </main>
