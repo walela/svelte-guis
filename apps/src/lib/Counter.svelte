5b5716197d7b1f5be5e7f291fba54305b2619a60
Austin Walela <austinmasibo@gmail.com>
2022-07-24 15:10:34 +0300

    tiny css fix

--- apps/src/lib/Counter.svelte
+++ apps/src/lib/Counter.svelte
@@ -12,12 +12,12 @@
       bind:value={count} />
     <button
       on:click={() => (count += 1)}
-      class="shadow-sm px-4 py-2 bg-blue-600 border rounded-md text-white text-medium hover:shadow-md hover:bg-blue-700">
+      class="shadow-sm px-6 py-2 bg-blue-600 border rounded text-white text-medium hover:shadow-md hover:bg-blue-500">
       Count
     </button>
   </div>
   <button
     on:click={() => (count = 0)}
-    class="shadow-sm px-4 py-2 bg-gray-200 border rounded-md text-gray-700 text-medium hover:shadow hover:bg-gray-300"
+    class="shadow-sm px-4 py-2 bg-gray-200 border rounded-md text-gray-700 text-medium hover:shadow"
     >Reset Count</button>
 </Layout>
