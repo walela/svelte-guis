97e5a135aa2a25511be17ed12d34e4afeeeeeeb2
Austin Walela <austinmasibo@gmail.com>
2022-07-24 12:34:39 +0300

    counter

--- apps/src/lib/Counter.svelte
+++ apps/src/lib/Counter.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
-  let count: number = 0
-  const increment = () => {
-    count += 1
-  }
+  import Layout from "./Layout.svelte";
+  let count: number = 0;
 </script>
 
-<button on:click={increment}>
-  count is {count}
-</button>
+<Layout title="Counter" width={96}>
+  <div class="flex gap-2 mb-4">
+    <input
+      type="text"
+      disabled
+      class="border px-2 text-gray-700 rounded"
+      bind:value={count} />
+    <button
+      on:click={() => (count += 1)}
+      class="shadow-sm px-4 py-2 bg-blue-600 border rounded-md text-white text-medium hover:shadow-md hover:bg-blue-700">
+      Count
+    </button>
+  </div>
+  <button
+    on:click={() => (count = 0)}
+    class="shadow-sm px-4 py-2 bg-gray-200 border rounded-md text-gray-700 text-medium hover:shadow hover:bg-gray-300"
+    >Reset Count</button>
+</Layout>
---
+++ apps/src/lib/Layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let title = "Hello, Svelte!";
+  export let width = 64;
+</script>
+
+<div class={`border w-${width} rounded shadow-sm pb-6`}>
+  <div class="bg-gray-100 py-2 mb-6 pl-6">
+    <h1 class="bold text-lg text-blue-600 uppercase">{title}</h1>
+  </div>
+  <div class="ml-6">
+    <slot />
+  </div>
+  
+</div>
