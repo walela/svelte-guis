3816762dbe4c66d9583f98793885903a47d059ac
Austin Walela <austinmasibo@gmail.com>
2022-07-26 11:23:18 +0300

    todo init

--- apps/src/App.svelte
+++ apps/src/App.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
   import gh from "./assets/gh.svg";
-  import Counter from "./lib/Counter.svelte";
+  import Counter from "./components/Counter.svelte";
+  import Todo from "./components/Todo.svelte";
 </script>
 
 <div class="w-3/4 mx-auto py-6 flex flex-col h-screen">
-  <main>
-    <h1 class="text-3xl text-center text-gray-700 extrabold mb-4">
-      Svelte GUIs
-    </h1>
+  <h1 class="text-3xl text-center text-gray-700 extrabold mb-4">
+    Svelte GUIs
+  </h1>
+  <main class="flex gap-3">
+
     <Counter />
+    <Todo />
   </main>
   <footer class="mt-auto mx-auto">
     <a href="https://github.com/walela/svelte-guis">
---
+++ apps/src/components/Todo.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Layout from "./Layout.svelte"
+</script>
+
+<Layout title="To Do" width={96}>
+    <input
+    type="text"
+    disabled
+    class="border px-2 text-gray-700 rounded"
+    bind:value={count} />
+</Layout>
