3382eae4e7eb7921c01292d1bfa3494f8783e36b
Austin Walela <austinmasibo@gmail.com>
2022-07-26 11:59:36 +0300

    error handling

--- apps/src/components/Todo.svelte
+++ apps/src/components/Todo.svelte
@@ -1,37 +1,44 @@
 <script lang="ts">
   import Layout from "./Layout.svelte";
-  interface Todo {
-    text: string;
-    completed: boolean;
-  }
+  import type { Todo } from "../types";
+
   let todo: Todo = { text: "", completed: false };
+  let error = "";
   let todos: Todo[] = [{ text: "Learn Svelte", completed: false }];
-  const addTask = todo => {
-    if (todo.text.trim()) {
-      todos.push(todo);
-      todos = todos;
-      todo = { text: "", completed: false };
+
+  $: {
+    if (todo.text) {
+      error = "";
     }
+  }
+  const addTask = () => {
+    if (todo.text.trim()) {
+      todos = [...todos, todo];
+    } else {
+      error = "Please enter a task";
+    }
+    todo = { text: "", completed: false };
   };
 </script>
 
 <Layout title="To Do" width={96}>
-  <div class="flex gap-2 mb-4">
+  <div class="flex gap-2 mb-2">
     <input
       type="text"
       class="border px-2 py-2 w-48 rounded focus:border-violet-500"
       bind:value={todo.text} />
     <button
-      on:click={() => addTask(todo)}
+      on:click={addTask}
       class="shadow-sm px-4 py-2 bg-gray-200 border rounded-md text-gray-700 text-medium hover:shadow"
       >Add Todo</button>
   </div>
+  <span class="text-red-400 mb-2">{error}</span>
   <ol>
     {#each todos as todo}
       <li
         on:click={() => (todo.completed = !todo.completed)}
         class={`${
-          todo.completed && "line-through"
+          todo.completed && "line-through text-gray-400"
         } text-gray-700 px-1 list-decimal ml-4 cursor-pointer hover:line-through hover:text-gray-400`}>
         {todo.text}
       </li>
