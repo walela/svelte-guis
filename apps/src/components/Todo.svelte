895bfaccd9d73311f5b0455033a306bcd16c3185
Austin Walela <austinmasibo@gmail.com>
2022-07-28 23:27:59 +0300

    refactor todo to use stores

--- apps/src/components/Todo.svelte
+++ apps/src/components/Todo.svelte
@@ -1,24 +1,7 @@
 <script lang="ts">
   import Layout from "./Layout.svelte";
-  import type { Todo } from "../types";
-
-  let todo: Todo = { text: "", completed: false };
-  let error = "";
-  let todos: Todo[] = [{ text: "Learn Svelte", completed: false }];
-
-  $: {
-    if (todo.text) {
-      error = "";
-    }
-  }
-  const addTask = () => {
-    if (todo.text.trim()) {
-      todos = [...todos, todo];
-    } else {
-      error = "Please enter a task";
-    }
-    todo = { text: "", completed: false };
-  };
+  import { todo, todos, count, addTodo, error } from "../stores";
+  console.log($error)
 </script>
 
 <Layout title="To Do" width={96}>
@@ -26,23 +9,23 @@
     <input
       type="text"
       class={`${
-        error && "border-2 border-red-400"
+        $error && "border-2 border-red-400"
       } border px-2 py-2 w-48 rounded focus:border-indigo-500`}
-      bind:value={todo.text} />
-    <button
-      on:click={addTask}
-      class="shadow-sm px-4 py-2 bg-gray-200 border rounded-md text-gray-700 text-medium hover:shadow"
-      >Add Todo</button>
+      bind:value={$todo.text} />
+    <button on:click={addTodo} class="shadow-sm px-4 py-2 bg-gray-200 border rounded-md text-gray-700 text-medium hover:shadow">Add Todo</button>
   </div>
-  <span class="text-red-400 mb-2">{error}</span>
+  {#if $error}
+    <span class="text-red-400 block mb-1">{$error}</span>
+  {/if}
+  <span class="text-gray-900 mb-2 extrabold">{$count} {$count === 1 ? "task" : "tasks"} left</span>
   <ol class="mt-2">
-    {#each todos as todo}
+    {#each $todos as todoItem}
       <li
-        on:click={() => (todo.completed = !todo.completed)}
+        on:click={() => (todoItem.completed = !todoItem.completed)}
         class={`${
-          todo.completed && "line-through text-gray-400"
+          todoItem.completed && "line-through text-gray-400"
         } text-gray-700 px-1 list-decimal ml-4 cursor-pointer hover:line-through hover:text-gray-400`}>
-        {todo.text}
+        {todoItem.text}
       </li>
     {/each}
   </ol>
---
+++ apps/src/stores.ts
@@ -0,0 +1,39 @@
+import type { Todo } from "./types";
+import type { Writable } from "svelte/store";
+import { writable, derived, get } from "svelte/store";
+
+export const todo: Writable<Todo> = writable({ text: "", completed: false });
+export const todos: Writable<Todo[]> = writable([
+  { text: "Learn Svelte", completed: false },
+]);
+
+const formState = writable({ status: "idle" });
+
+export const addTodo = () => {
+  formState.set({ status: "submitted" });
+  let _error = get(error);
+  if (!_error) {
+    let _todo = get(todo);
+    todos.update(todos => {
+      todos.push(_todo);
+      todo.update(() => ({ text: "", completed: false }));
+      return todos;
+    });
+    formState.set({ status: "idle" });
+  }
+};
+
+export const count = derived(todos, todos => {
+  return todos.filter(todo => !todo.completed).length;
+});
+
+export const error = derived([todo, formState], $values => {
+  if ($values[1].status === "submitted") {
+    if (!$values[0].text.trim()) {
+      return "Please enter a task";
+    } else if ($values[0].text.length < 3) {
+      return "Todo must be at least 3 characters long";
+    }
+    return "";
+  }
+});
